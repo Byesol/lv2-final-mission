5c81ca63ee3dd2dda12391ae77e631ec2a1120e7
[CLOUD4] 백승주 <bsj5967@naver.com>
2025-06-18 18:34:43 +0900

    feat: 도서 검색 통합 테스트 구현

---
+++ src/test/java/library/e2e/BookSearchE2ETest.java
@@ -0,0 +1,51 @@
+package library.e2e;
+
+import static org.hamcrest.Matchers.*;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@TestPropertySource(properties = {
+    "naver.client-id=KsCtXhM1Feo7P0K24Oka",
+    "naver.client-secret=e7m7DCN1vn"
+})
+class BookSearchE2ETest {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @Test
+    @DisplayName("GET /books/search로 책을 검색할 수 있다")
+    void searchBooks_성공() {
+        // given
+        String query = "자바";
+
+        // when & then
+        RestAssured.given().log().all()
+                .queryParam("query", query)
+                .when().get("/books/search")
+                .then().log().all()
+                .statusCode(200)
+                .body("items", notNullValue())
+                .body("items", hasSize(greaterThan(0)))
+                .body("items[0].title", notNullValue())
+                .body("items[0].author", notNullValue())
+                .body("items[0].isbn", notNullValue())
+                .body("items[0].description", notNullValue())
+                .body("items[0].title", containsString("자바"));
+    }
+}
